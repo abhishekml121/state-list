a286f7c40c199457e39148a50fd4e0bf586e71f6
Abhishek kamal <42137990+abhishekml121@users.noreply.github.com>
2019-06-05 11:22:41 +0530

    Add files via upload

---
+++ states_list.php
@@ -0,0 +1,158 @@
+<?php
+
+$categories = [
+[
+    'id' => 1, 'country_name' => 'Argentina', 'states' => [
+      ['id' => 1, 'name' => 'Catamarca'],
+      ['id' => 2, 'name' => 'Chaco'],
+      ['id' => 3, 'name' => 'Chubut'],
+      ['id' => 4, 'name' => 'Ciudad De Buenos Aires'],
+      ['id' => 5, 'name' => 'Cordoba'],
+      ['id' => 6, 'name' => 'Corrientes'],
+      ['id' => 7, 'name' => 'Entre Rios'],
+      ['id' => 8, 'name' => 'Formosa'],
+      ['id' => 9, 'name' => 'Jujuy'],
+      ['id' => 10, 'name' => 'La Pampa'],
+      ['id' => 11, 'name' => 'La Rioja'],
+      ['id' => 12, 'name' => 'Mendoza'],
+      ['id' => 13, 'name' => 'Misiones'],
+      ['id' => 14, 'name' => 'Neuquen'],
+      ['id' => 15, 'name' => 'Rio Negro'],
+      ['id' => 16, 'name' => 'Salta'],
+      ['id' => 17, 'name' => 'Kentucky'],
+      ['id' => 18, 'name' => 'San Juan'],
+      ['id' => 19, 'name' => 'San Luis'],
+      ['id' => 20, 'name' => 'Santa Cruz'],
+      ['id' => 21, 'name' => 'Santa Fe'],
+      ['id' => 22, 'name' => 'Santiago Del Estero'],
+      ['id' => 23, 'name' => 'Tierra Del Fuego'],
+      ['id' => 24, 'name' => 'Tucuman']
+    ]
+  ],
+
+  [
+    'id' => 2, 'country_name' => 'Australia', 'states' => [
+      ['id' => 1, 'name' => 'Australian Capital Territory'],
+      ['id' => 2, 'name' => 'New South Wales'],
+      ['id' => 3, 'name' => 'Northern Territory'],
+      ['id' => 4, 'name' => 'Queensland'],
+      ['id' => 5, 'name' => 'South Australia'],
+      ['id' => 6, 'name' => 'Tasmania'],
+      ['id' => 7, 'name' => 'Victoria'],
+      ['id' => 8, 'name' => 'Western Australia']
+    ]
+  ],
+  
+  [
+    'id' => 3, 'country_name' => 'India', 'states' => [
+      ['id' => 1, 'name' => 'Andhra Pradesh'],
+      ['id' => 2, 'name' => 'Arunachal Pradesh'],
+      ['id' => 3, 'name' => 'Assam'],
+      ['id' => 4, 'name' => 'Bihar'],
+      ['id' => 5, 'name' => 'Chhattisgarh'],
+      ['id' => 6, 'name' => 'Goa'],
+      ['id' => 7, 'name' => 'Gujrat'],
+      ['id' => 8, 'name' => 'Haryana'],
+      ['id' => 9, 'name' => 'Himachal Pradesh'],
+      ['id' => 10, 'name' => 'Jammu & Kashmir'],
+      ['id' => 11, 'name' => 'Jharkhand'],
+      ['id' => 12, 'name' => 'Karnataka'],
+      ['id' => 13, 'name' => 'Madhya Pradesh'],
+      ['id' => 14, 'name' => 'Maharashtra'],
+      ['id' => 15, 'name' => 'Manipur'],
+      ['id' => 16, 'name' => 'Meghalaya'],
+      ['id' => 17, 'name' => 'Mizoram'],
+      ['id' => 18, 'name' => 'Nagaland'],
+      ['id' => 19, 'name' => 'Odisha'],
+      ['id' => 20, 'name' => 'Punjab'],
+      ['id' => 21, 'name' => 'Rajasthan'],
+      ['id' => 22, 'name' => 'Sikkim'],
+      ['id' => 23, 'name' => 'Tamil Nadu
+'],
+      ['id' => 24, 'name' => 'Telangana'],
+      ['id' => 25, 'name' => 'Tripura'],
+      ['id' => 26, 'name' => 'Uttar Pradesh'],
+      ['id' => 27, 'name' => 'Uttarakhand'],
+      ['id' => 28, 'name' => 'Uttar Pradesh'],
+      ['id' => 29, 'name' => 'West Bengal']
+    ]
+  ],
+
+  [
+    'id' => 4, 'country_name' => 'United states of America', 'states' => [
+      ['id' => 1, 'name' => 'Alabama'],
+      ['id' => 2, 'name' => 'Alaska'],
+      ['id' => 3, 'name' => 'Arizona'],
+      ['id' => 4, 'name' => 'Arkansas'],
+      ['id' => 5, 'name' => 'California'],
+      ['id' => 6, 'name' => 'Colorado'],
+      ['id' => 7, 'name' => 'Connecticut'],
+      ['id' => 8, 'name' => 'Delaware'],
+      ['id' => 9, 'name' => 'Florida'],
+      ['id' => 10, 'name' => 'Georgia'],
+      ['id' => 11, 'name' => 'Hawaii'],
+      ['id' => 12, 'name' => 'Idaho'],
+      ['id' => 13, 'name' => 'Illinois'],
+      ['id' => 14, 'name' => 'Indiana'],
+      ['id' => 15, 'name' => 'Iowa'],
+      ['id' => 16, 'name' => 'Kansas'],
+      ['id' => 17, 'name' => 'Kentucky'],
+      ['id' => 18, 'name' => 'Louisiana'],
+      ['id' => 19, 'name' => 'Maine'],
+      ['id' => 20, 'name' => 'Maryland'],
+      ['id' => 21, 'name' => 'Massachusetts'],
+      ['id' => 22, 'name' => 'Michigan'],
+      ['id' => 23, 'name' => 'Minnesota'],
+      ['id' => 24, 'name' => 'Michigan'],
+      ['id' => 25, 'name' => 'Mississippi'],
+      ['id' => 26, 'name' => 'Missouri'],
+      ['id' => 27, 'name' => 'Montana'],
+      ['id' => 28, 'name' => 'Nebraska'],
+      ['id' => 29, 'name' => 'Nevada'],
+      ['id' => 30, 'name' => 'New Hampshire'],
+      ['id' => 31, 'name' => 'New Jersey'],
+      ['id' => 32, 'name' => 'New Mexico'],
+      ['id' => 33, 'name' => 'New York'],
+      ['id' => 34, 'name' => 'North Carolina'],
+      ['id' => 35, 'name' => 'North Dakota'],
+      ['id' => 36, 'name' => 'Ohio'],
+      ['id' => 37, 'name' => 'Oklahoma'],
+      ['id' => 38, 'name' => 'Oregon'],
+      ['id' => 39, 'name' => 'Pennsylvania'],
+      ['id' => 40, 'name' => 'Rhode Island'],
+      ['id' => 41, 'name' => 'South Carolina'],
+      ['id' => 42, 'name' => 'South Dakota'],
+      ['id' => 43, 'name' => 'Tennessee'],
+      ['id' => 44, 'name' => 'Utah'],
+      ['id' => 45, 'name' => 'Vermont'],
+      ['id' => 46, 'name' => 'Virginia'],
+      ['id' => 47, 'name' => 'Washington'],
+      ['id' => 48, 'name' => 'West Virginia'],
+      ['id' => 49, 'name' => 'Wisconsin'],
+      ['id' => 50, 'name' => 'Wyoming']
+    ]
+  ]
+
+  
+];
+
+
+ $category_id = isset($_GET['category_id']) ? (int) $_GET['category_id'] : 0;
+
+  foreach($categories as $category) {
+    if($category['id'] == $category_id) {
+
+      // Code when this id matched requested id
+      $states = $category['states'];
+      foreach($states as $state) {
+        echo "<option value=\"{$state['id']}\">";
+        echo $state['name'];
+        echo "</option>";
+      }
+
+
+    }
+  }
+
+
+?>
